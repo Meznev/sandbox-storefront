527ff2fd84cb0bc97f2c5a941c29be98835f97f0
Axel31 <amigy@datasolution.fr>
2025-04-04 17:52:49 +0200

    test error add to cart from gift fragment

--- scripts/__dropins__/storefront-cart/fragments.js
+++ scripts/__dropins__/storefront-cart/fragments.js
@@ -166,11 +166,6 @@ const e=`
       name
       sku
       gift_message_available
-      gift_wrapping_available
-      gift_wrapping_price {
-        currency
-        value
-      }
       thumbnail {
         url
         label
